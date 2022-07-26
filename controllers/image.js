395a1ccd6d1e0991fca434d465aa53c1818e4a92
Tellula <julieloane@gmail.com>
2022-07-26 18:05:24 +0200

    Add clarifai

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const Clarifai = require("clarifai");
+require ('custom-env').env('id')
+
+let app = new Clarifai.App({
+  apiKey: process.env.CLARIFAI_KEY,
+});
+  
+
+const handleApiCall = (req, res) => {
+  app.models
+  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -12,4 +29,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleApiCall,
 };
